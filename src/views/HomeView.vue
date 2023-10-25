c42c09afdff37aafb222f8adfc81db8a4e6352bc
bboggo <kmt0819@naver.com>
2023-10-26 00:15:37 +0900

    update css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,7 +22,7 @@
     <div class="container">
       <div class="main">
         <h3>{{ subtitle }}</h3>
-      <h1>{{ name }}</h1>
+        <h1>{{ name }}</h1>
       </div>
 
       <div class="aboutme">
@@ -38,31 +38,41 @@
             <v-carousel height="500" hide-delimiters progress="primary">
             <v-carousel-item>
               <div class="infor-text1">
-                <h3>자기소개</h3>
-                <p>이름 : 김민지</p>
+                <h3>My Information</h3>
+                <p>이름 | 김민지</p>
                 <p>생년월일 : 2000.08.19</p>
-                <p>학력 : 명지전문대학 정보통신공학과 졸업</p>
-                <p> : 명지전문대학 정보통신공학과 졸업</p>
+                <p>학력</p>
+                <p>2023.02 | 명지전문대학 정보통신공학과 졸업</p>
+                <p>2023.03 ~ | 명지전문대학 정보통신공학과 전공심화 재학중</p>
               </div>
             </v-carousel-item>
 
             <v-carousel-item>
               <div class="infor-text1">
-                <h3>자기소개2</h3>
-                <p>이름 : 김민지</p>
-                <p>생년월일 : 2000.08.19</p>
-                <p>학력 : 명지전문대학 정보통신공학과 졸업</p>
-                <p> : 명지전문대학 정보통신공학과 졸업</p>
+                <h3>Certificate</h3>
+                <p>2022.04 | 리눅스 마스터 2급 | 한국정보통신인력개발센터</p>
+                <p>2022.09 | 정보처리산업기사 | 한국산업인력공단</p>
+                <p>2023.09 | 정보처리기사 | 한국산업인력공단</p>
+                <br>
+
+                <p>2022.11 | 명지전문대학 정보통신공학과 동상</p>
+
+                <p>2022.12 | 명지전문대학 캡스톤디자인대회 동상</p>
               </div>
             </v-carousel-item>
 
             <v-carousel-item>
               <div class="infor-text1">
-                <h3>Contact</h3>
-                <p>이름 : 김민지</p>
-                <p>생년월일 : 2000.08.19</p>
-                <p>학력 : 명지전문대학 정보통신공학과 졸업</p>
-                <p> : 명지전문대학 정보통신공학과 졸업</p>
+                <h3>Contact & Award</h3>
+                
+                <p>E-mail: kmt0819@naver.com</p>
+                <p>Phone: 010-3958-3589</p>
+                <a href="https://github.com/bboggo">github</a>
+
+                <a href="https://velog.io/@bboggo">blog</a>
+
+                <a href="https://purring-pasta-b76.notion.site/Kim-Min-Ji-567d94a278a04158a36e58ea5dfb1be1?pvs=4">notion</a>
+
               </div>
             </v-carousel-item>
           </v-carousel>
@@ -72,7 +82,7 @@
       </div>
 
       <div class="text">
-        <p>⭐ 내가 왜 개발자를 하고 싶었는지</p>
+        <p>⭐ 백엔드 개발자가 되고 싶은 김민지입니다. ⭐</p>
         <p>⭐ 나에 대한 소개입니다.</p>
         <p>⭐ 나에 대한 소개입니다.</p> 
       </div>
@@ -132,22 +142,11 @@
           <h1>Experience</h1>
         </div>
         <div class="ex-text">
-          아르바이트, 교육 등 다양한 경험을 해보았습니다.
+          수상 및 자격증, 프로젝트 제외 활동 내역입니다. 아르바이트, 교육 등 다양한 경험을 해보았습니다.
         </div>
         <div class="ex-list-grid">
           <div class="ex-list">
-            <h2>Alpha mini 아르바이트</h2>
-            <p>2023-10-21~</p>
-          </div>
-          <div class="ex-list">
-            <li>아르바이트, 교육 등 다양한 경험을 해보았습니다. 이 과정에서 많은 것을 배울 수 있었고 많은 성장을 할 수 있었습니다.</li>
-            <li>아르바이트, 교육 등 다양한 경험을 해보았습니다. 이 과정에서 많은 것을 배울 수 있었고 많은 성장을 할 수 있었습니다.</li>
-          </div>
-        </div>
-        <hr>
-        <div class="ex-list-grid">
-          <div class="ex-list">
-            <h2>Alpha mini 아르바이트</h2>
+            <h2>코드잇 강의 수강</h2>
             <p>2023-10-21~</p>
           </div>
           <div class="ex-list">
@@ -158,7 +157,7 @@
         <hr>
         <div class="ex-list-grid">
           <div class="ex-list">
-            <h2>Alpha mini 아르바이트</h2>
+            <h2>4차 산업 연구회 동아리</h2>
             <p>2023-10-21~</p>
           </div>
           <div class="ex-list">
@@ -169,7 +168,7 @@
         <hr>
         <div class="ex-list-grid">
           <div class="ex-list">
-            <h2>Alpha mini 아르바이트</h2>
+            <h2>한이음 ICT 멘토링</h2>
             <p>2023-10-21~</p>
           </div>
           <div class="ex-list">
@@ -180,7 +179,29 @@
         <hr>
         <div class="ex-list-grid">
           <div class="ex-list">
-            <h2>Alpha mini 아르바이트</h2>
+            <h2>전공 튜터링</h2>
+            <p>2023-10-21~</p>
+          </div>
+          <div class="ex-list">
+            <li>아르바이트, 교육 등 다양한 경험을 해보았습니다. 이 과정에서 많은 것을 배울 수 있었고 많은 성장을 할 수 있었습니다.</li>
+            <li>아르바이트, 교육 등 다양한 경험을 해보았습니다. 이 과정에서 많은 것을 배울 수 있었고 많은 성장을 할 수 있었습니다.</li>
+          </div>
+        </div>
+
+        <div class="ex-list-grid">
+          <div class="ex-list">
+            <h2>학습법 특강</h2>
+            <p>2023-10-21~</p>
+          </div>
+          <div class="ex-list">
+            <li>아르바이트, 교육 등 다양한 경험을 해보았습니다. 이 과정에서 많은 것을 배울 수 있었고 많은 성장을 할 수 있었습니다.</li>
+            <li>아르바이트, 교육 등 다양한 경험을 해보았습니다. 이 과정에서 많은 것을 배울 수 있었고 많은 성장을 할 수 있었습니다.</li>
+          </div>
+        </div>
+
+        <div class="ex-list-grid">
+          <div class="ex-list">
+            <h2>Alpha mini 교재 번역 아르바이트</h2>
             <p>2023-10-21~</p>
           </div>
           <div class="ex-list">
@@ -190,6 +211,50 @@
         </div>
       </div>
 
+      
+
+      <div class="modal-background" v-if="showModal" @click="closeModal"></div>
+      <div>
+        <!-- 모달 창 -->
+        <div class="modal" v-if="showModal">
+          <!-- 모달 내용 -->
+          <div class="modal-content">
+            <div class="project-infor">
+              <h2>학원 관리 시스템</h2>
+              <p>진행 기간 : 2023-10~2023-1012</p>
+              <p class="cate">웹 사이트</p>
+              <a href="">github</a>
+            </div>
+            <div class="project-grid">
+              <div class="project-image">
+                <v-img class="my-pic" src='@/assets/smartfarm.png'></v-img>
+              </div>
+              <div class="project-des">
+                어떤 프로젝트인지 기획에 대해서 설명
+              </div>
+            </div>
+            <div class="project-my">
+              <h3>내가 담당한 부분 : 백엔드</h3>
+              
+            </div>
+            <div class="project-skill">
+              <h3>기술 스택</h3>
+              <li>Spring Boot</li>
+              <li>Spring Boot</li>
+              <li>Spring Boot</li>
+            </div>
+
+            <div class="project-feel">
+              <h3>느낀점</h3>
+              <p>어떤 점을 느꼈는지 아주 자세하게 쓰려고함ㅁ. 진짜 자세하게 학원관리 시스템을 만들면서 무슨 감정을 느꼈고 그래봤자 안좋은 감정이지 뭐</p>
+            </div>
+
+            <!-- 모달 내용 추가 -->
+
+            <button @click="closeModal">닫기</button>
+          </div>
+        </div>
+      </div>
 
       <div class="portfolio">
         <div class="title">
@@ -197,7 +262,12 @@
         </div>
         <div class="portfolio-grid">
 
-          <div class="port" data-aos="fade-up" data-aos-duration="4000">
+          <div class="port"
+         data-aos="fade-up"
+         data-aos-duration="4000"
+         @mouseenter="activatePort"
+         @mouseleave="deactivatePort"
+         @click="openModal">
             <v-img class="my-pic" src='@/assets/node.png'></v-img>
             <div class="port-text">
               <h3>학원관리 시스템</h3>
@@ -208,7 +278,7 @@
           </div>
 
           <div class="port" ddata-aos="fade-up" data-aos-duration="4000">
-            <v-img class="my-pic" src='@/assets/node.png'></v-img>
+            <v-img class="my-pic" src='@/assets/smartfarm.png'></v-img>
             <div class="port-text">
               <h3>학원관리 시스템</h3>
               <p>2023-10-21~</p>
@@ -218,7 +288,7 @@
           </div>
 
           <div class="port" data-aos="fade-up" data-duration="2000">
-            <v-img class="my-pic" src='@/assets/node.png'></v-img>
+            <v-img class="port-img" src='@/assets/node.png'></v-img>
             <div class="port-text">
               <h3>학원관리 시스템</h3>
               <p>2023-10-21~</p>
@@ -255,16 +325,18 @@ export default {
   data: () => ({
     subtitle: '',
     name: '',
+    showModal: false,
+    isActivated: false
   }),
 
   mounted() {
-    this.typeText('노력하고 뭐시기하는 개발자', 'subtitle')
+    this.typeText('백엔드 개발자', 'subtitle')
       .then(() => {
-        this.subtitle = '노력하고 뭐시기하는 개발자';
-        return this.typeText('김민지입니다.', 'name');
+        this.subtitle = '백엔드 개발자';
+        return this.typeText('김민지의 포트폴리오입니다.', 'name');
       })
       .then(() => {
-        this.name = '김민지입니다.';
+        this.name = '김민지의 포트폴리오입니다.';
       });
   },
 
@@ -286,6 +358,7 @@ export default {
 
         type();
       });
+      
     },
     scrollToSection(section) {
       // 해당 섹션의 DOM 요소를 선택
@@ -295,7 +368,19 @@ export default {
         element.scrollIntoView({ behavior: 'smooth' });
       }
     },
-  },
+    openModal() {
+      this.showModal = true; // 모달 창 열기
+    },
+    closeModal() {
+      this.showModal = false; // 모달 창 닫기
+    },
+    activatePort() {
+      this.isActivated = true;
+    },
+    deactivatePort() {
+      this.isActivated = false;
+    },
+  }
 };
 
 </script>
@@ -335,6 +420,13 @@ export default {
   height: 800px;
 }
 
+.main h1 {
+  font-size: 40px;
+}
+
+.main h3 {
+  font-size: 25px;
+}
 .container {
   margin: 0 auto;
 }
@@ -361,22 +453,31 @@ export default {
 }
 
 .infor-text1 {
-  padding: 100px 150px;
-  height: 100%;
+  padding: 70px 100px;
+  height: 90%;
   background-color: rgb(255, 251, 242);
 }
 
 .infor-text1 h3 {
   font-size: 30px;
-  color: rgb(48, 48, 48);
-  margin-bottom: 70px;
+  color: rgb(222, 173, 77);
+  margin-bottom: 50px;
 }
 
 .infor-text1 p {
   margin-bottom: 10px;
-  font-size: 20px;
+  font-size: 18px;
+  font-weight: 500;
+  padding-bottom: 10px;
 }
 
+.infor-text1 a {
+  background-color: black;
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  margin-right: 10px;
+}
 .title {
   font-size: 20px;
   text-align: center;
@@ -471,7 +572,7 @@ export default {
 .portfolio-grid {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 150px;
+  gap: 100px;
 }
 
 .portfolio-grid .port {
@@ -489,6 +590,8 @@ img:hover {
 .port {
   box-shadow: 5px 5px 5px 5px rgb(235, 235, 235);
 }
+
+
 .port-text {
   padding: 10px;
   font-weight: 400;
@@ -510,6 +613,59 @@ footer {
   background-color: rgb(244, 216, 159);
   text-align: center;
   margin-top: 100px;
+}
+
+.modal-background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7); /* 검정색 배경 및 투명도 조절 */
+  z-index: 999; /* 다른 요소 위에 표시되도록 z-index 설정 */
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  z-index: 1000; /* 뒷 배경보다 위에 표시 */
+  width: 700px;
+  height: 700px;
+}
+
+.modal .cate {
+  margin-bottom: 10px;
+
+}
+.modal a {
+  background-color: black;
+  color: white;
+  padding: 10px;
+  margin: 20px 0;
+  border-radius: 10px;
+}
+
+.modal img {
+  margin-top: 20px;
+}
+
+.modal h3 {
+  margin: 10px 0;
+}
+.modal-content {
+  /* 모달 내용 스타일 설정 */
+}
+
+.project-grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
 }
 
 </style>
