cea78cf04cc9d24a263974af458a968c090f8962
bboggo <kmt0819@naver.com>
2023-10-26 02:32:16 +0900

    modify public path

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ const path = require("path")
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: "/portfolio_upload/",
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
